5772ad4af75f42b1dabf59c45cbb2e79cb03c092
DevloperKaran <dailydosebykaran77@gmail.com>
2024-06-18 10:38:55 +0530

    Python

---
+++ counter.ai.py
@@ -0,0 +1,16 @@
+
+
+
+print("Welcome to counter.ai")
+
+a = input("Type Your Name Here :")
+
+print("Hello", a )
+
+print("The Indias First Word Couter Ai Website Welcome's You")
+x = input("Type your Data :")
+len1 = len(x)
+print("Your Data Contain", len1 ,"Letter")
+
+#for charcter in x
+#print(character)
